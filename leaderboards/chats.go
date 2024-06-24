81b8d23cfa3d5905b283d859b54daa3e551e5002
bla <92544965+blableblup@users.noreply.github.com>
2024-06-24 15:44:15 +0200

    global boards, fixed pfp link

--- leaderboards/chats.go
+++ leaderboards/chats.go
@@ -135,7 +135,7 @@ func RunChatStatsGlobal(params LeaderboardParams) {
 			continue
 		}
 
-		chatInfo.TypeName = fmt.Sprintf("![%s](../../images/players/%s.png)", chatName, chatName)
+		chatInfo.TypeName = fmt.Sprintf("![%s](https://raw.githubusercontent.com/blableblup/gofish/main/images/players/%s.png)", chatName, chatName)
 		chatInfo.Chat = chatName
 
 		// Update chatStats
