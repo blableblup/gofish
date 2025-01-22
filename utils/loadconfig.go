d88569ac27cf8625d34aec2a92f0d7aa3a5df7f9
bla <92544965+blableblup@users.noreply.github.com>
2025-01-22 19:03:08 +0100

    config: add other justlog instances

--- utils/loadconfig.go
+++ utils/loadconfig.go
@@ -8,18 +8,18 @@ import (
 )
 
 type ChatInfo struct {
-	Fishweeklimit      int     `json:"fishweek_limit"`
-	Weightlimit        float64 `json:"weight_limit"`
-	WeightlimitRecords float64 `json:"weight_limit_records"`
-	Totalcountlimit    int     `json:"totalcount_limit"`
-	Uniquelimit        int     `json:"unique_limit"`
-	Rowlimit           int     `json:"row_limit"`
-	LogsHost           string  `json:"logs_host"`
-	LogsHostOld        string  `json:"logs_host_old"`
-	LogsAdded          string  `json:"logs_added"`
-	Emoji              string  `json:"emoji"`
-	CheckFData         bool    `json:"checkfdata"`
-	BoardsEnabled      bool    `json:"board_enabled"`
+	Fishweeklimit      int               `json:"fishweek_limit"`
+	Weightlimit        float64           `json:"weight_limit"`
+	WeightlimitRecords float64           `json:"weight_limit_records"`
+	Totalcountlimit    int               `json:"totalcount_limit"`
+	Uniquelimit        int               `json:"unique_limit"`
+	Rowlimit           int               `json:"row_limit"`
+	LogsHost           string            `json:"logs_host"`
+	LogsHostAlts       map[string]string `json:"logs_host_alts"`
+	LogsAdded          string            `json:"logs_added"`
+	Emoji              string            `json:"emoji"`
+	CheckFData         bool              `json:"checkfdata"`
+	BoardsEnabled      bool              `json:"board_enabled"`
 }
 
 type Config struct {
@@ -30,25 +30,25 @@ func LoadConfig() Config {
 
 	wd, err := os.Getwd()
 	if err != nil {
-		logs.Logs().Fatal().Err(err).Msg("Error getting current working directory")
+		logs.Logs().Fatal().Err(err).
+			Msg("Error getting current working directory")
 	}
 
 	configFilePath := filepath.Join(wd, "config.json")
-	logs.Logs().Debug().Str("configFilePath", configFilePath).Msg("Loading config file")
 
 	file, err := os.Open(configFilePath)
 	if err != nil {
-		logs.Logs().Fatal().Err(err).Msg("Error opening config file")
+		logs.Logs().Fatal().Err(err).
+			Msg("Error opening config file")
 	}
 	defer file.Close()
 
 	var config Config
 	err = json.NewDecoder(file).Decode(&config)
 	if err != nil {
-		logs.Logs().Fatal().Err(err).Msg("Error parsing config file")
+		logs.Logs().Fatal().Err(err).
+			Msg("Error parsing config file")
 	}
-
-	logs.Logs().Debug().Interface("config", config).Msg("Loaded chat config")
 
 	return config
 }
