b32548f91263384b8e07bad89914e98c81faf7fb
bla <92544965+blableblup@users.noreply.github.com>
2025-02-28 16:08:13 +0100

    url: can also now select instances by name

--- data/url.go
+++ data/url.go
@@ -10,10 +10,7 @@ import (
 )
 
 // This creates the urls which get checked in data. By default it returns the url of the current month
-func CreateURL(chatName string, numMonths int, monthYear string, logInstance string, config utils.Config) []string {
-
-	var urls []string
-	var justlogInstance, justlogInstanceAdded string
+func CreateURL(chatName string, chat utils.ChatInfo, numMonths int, monthYear string, logInstance string) []string {
 
 	// If a different date was specified, update timevar, else use time.Now in UTC, because all the instances are in UTC
 	timevar := time.Now().UTC()
@@ -40,42 +37,59 @@ func CreateURL(chatName string, numMonths int, monthYear string, logInstance str
 		timevar = time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
 	}
 
-	// Get the selected instances, like:  -instance 1,2; default instance is "0"
+	if len(chat.LogsInstances) == 0 {
+		logs.Logs().Fatal().
+			Str("Chat", chatName).
+			Msg("No logs host specified for chat in config !")
+	}
+
+	// Get the selected instances, default instance is "0" which is the first in the slice
+	// Can be the number of the instance in the config slice, or the name of the instance; like -instance 2,ivr,potat
+	// Double selecting the same instance doesnt work; that will just overwrite selectedInstances[...]...
 	// But checking multiple instances for the same chat at the same time will just add the same fish multiple times
 	// Even with mode "a",(if the fish was missing in the db), so this is kinda useless ?
-	var selectedInstances []int
+	// Thats also why there is no -instance all, or maybe could update data to check if the fish also exists in the fish slice instead of only in the db
+	// to remove duplicates from the other instances ?
+	selectedInstances := make(map[string]string)
 	instances := strings.Split(logInstance, ",")
 	for a := range instances {
 		instance, err := strconv.Atoi(instances[a])
 		if err != nil {
-			logs.Logs().Fatal().
-				Str("Chat", chatName).
-				Interface("Selected instances", logInstance).
-				Msg("Can't convert instance to int")
+			// Check if the name of an instance was selected instead
+			thatinstanceexists := false
+			for _, existinginstance := range chat.LogsInstances {
+				if strings.Contains(existinginstance.URL, instances[a]) {
+					selectedInstances[existinginstance.URL] = existinginstance.LogsAdded
+					thatinstanceexists = true
+					break
+				}
+			}
+			if !thatinstanceexists {
+				logs.Logs().Fatal().
+					Str("Chat", chatName).
+					Str("Instance", instances[a]).
+					Msg("Selected instance does not exist")
+			}
+		} else {
+			// If err nil, select the element of the slice as the instance
+			if len(chat.LogsInstances) <= instance {
+				logs.Logs().Fatal().
+					Str("Chat", chatName).
+					Int("Selected instance", instance).
+					Interface("Available instances", chat.LogsInstances).
+					Msg("Chat does not have that many different instances !")
+			}
+			selectedInstances[chat.LogsInstances[instance].URL] = chat.LogsInstances[instance].LogsAdded
 		}
-		selectedInstances = append(selectedInstances, instance)
 	}
 
-	for _, instance := range selectedInstances {
+	// Go over the instances and create the urls
+	var urls []string
 
-		if len(config.Chat[chatName].LogsInstances) == 0 {
-			logs.Logs().Fatal().
-				Str("Chat", chatName).
-				Msg("No logs host specified for chat in config !")
-		}
-		if len(config.Chat[chatName].LogsInstances) <= instance {
-			logs.Logs().Fatal().
-				Str("Chat", chatName).
-				Int("Selected instance", instance).
-				Interface("Available instances", config.Chat[chatName].LogsInstances).
-				Msg("Chat does not have that many different instances !")
-		}
-
-		justlogInstance = config.Chat[chatName].LogsInstances[instance].URL
-		justlogInstanceAdded = config.Chat[chatName].LogsInstances[instance].LogsAdded
+	for justlogInstance, justlogInstanceAdded := range selectedInstances {
 
 		// Loop through the specified number of months
-		for i := 0; i < numMonths; i++ {
+		for i := range numMonths {
 
 			// Get the year and month to check, from the first day of that month, because idk ?
 			firstOfMonth := time.Date(timevar.Year(), timevar.Month()-time.Month(i), 1, 0, 0, 0, 0, time.UTC)
