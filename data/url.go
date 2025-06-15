88cd015e4c75f255c0a118410d8fde1449e86d52
bla <92544965+blableblup@users.noreply.github.com>
2025-06-15 15:30:29 +0200

    url: use userid instead of name

--- data/url.go
+++ data/url.go
@@ -121,23 +121,27 @@ func CreateURL(chatName string, chat utils.ChatInfo, numMonths int, monthYear st
 				}
 			}
 
+			// the twitchids of the bots:
+			supibotID := "68136884"
+			gofishgameID := "951349582"
+
 			// Check if the current month is within September 2023
 			if year == 2023 && month == time.September {
 
 				// Check supi and gofishgame
-				urlSupi := fmt.Sprintf("%s/channelid/%s/user/supibot/%d/%d?", justlogInstance, chat.TwitchID, year, int(month))
-				urlGofi := fmt.Sprintf("%s/channelid/%s/user/gofishgame/%d/%d?", justlogInstance, chat.TwitchID, year, int(month))
+				urlSupi := fmt.Sprintf("%s/channelid/%s/userid/%s/%d/%d?", justlogInstance, chat.TwitchID, supibotID, year, int(month))
+				urlGofi := fmt.Sprintf("%s/channelid/%s/userid/%s/%d/%d?", justlogInstance, chat.TwitchID, gofishgameID, year, int(month))
 				urls = append(urls, urlSupi, urlGofi)
 			} else {
 				// Check supi for months before september 2023, check gofishgame for months after september 2023
 				if year < 2023 || (year == 2023 && month < time.September) {
 
-					url := fmt.Sprintf("%s/channelid/%s/user/supibot/%d/%d?", justlogInstance, chat.TwitchID, year, int(month))
+					url := fmt.Sprintf("%s/channelid/%s/userid/%s/%d/%d?", justlogInstance, chat.TwitchID, supibotID, year, int(month))
 					urls = append(urls, url)
 
 				} else {
 
-					url := fmt.Sprintf("%s/channelid/%s/user/gofishgame/%d/%d?", justlogInstance, chat.TwitchID, year, int(month))
+					url := fmt.Sprintf("%s/channelid/%s/userid/%s/%d/%d?", justlogInstance, chat.TwitchID, gofishgameID, year, int(month))
 					urls = append(urls, url)
 				}
 			}
