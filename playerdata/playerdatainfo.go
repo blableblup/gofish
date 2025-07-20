46a1d1f54f4226dc31100fb0a3ced238f10cb692
bla <92544965+blableblup@users.noreply.github.com>
2025-07-20 13:30:14 +0200

    use logger in old code

--- playerdata/playerdatainfo.go
+++ playerdata/playerdatainfo.go
@@ -2,40 +2,36 @@ package playerdata
 
 import (
 	"bufio"
-	"fmt"
+	"gofish/logs"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
 )
 
-// ReadVerifiedPlayers reads verified players from a text file
+// for verified script
 func ReadVerifiedPlayers() []string {
 	verifiedPlayers := make([]string, 0)
 
-	// Get the directory of the source file
 	_, filename, _, _ := runtime.Caller(0)
 	dir := filepath.Dir(filename)
 
-	// Construct the path to the verified.txt file relative to the source file directory
 	verifiedPath := filepath.Join(dir, "verified.txt")
 
 	file, err := os.Open(verifiedPath)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
+		logs.Logs().Error().Err(err).Msg("Error opening file")
 		return verifiedPlayers
 	}
 	defer file.Close()
 
-	// Read each line from the text file
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		// Append the line to the list of verified players after stripping any leading or trailing whitespace
 		verifiedPlayers = append(verifiedPlayers, strings.TrimSpace(scanner.Text()))
 	}
 
 	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
+		logs.Logs().Error().Err(err).Msg("Error reading file")
 		return verifiedPlayers
 	}
 
